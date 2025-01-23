518550a33ece7050cb1cc59098a816312340b34f
Bartu10 <jbg10102002@gmail.com>
2025-01-23 23:18:52 +0100

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path: '',component: HomeComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'tasks', component: TasksComponent},
+  {path: 'tasks/:id', component: UsersComponent},
+
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+
+
+];
