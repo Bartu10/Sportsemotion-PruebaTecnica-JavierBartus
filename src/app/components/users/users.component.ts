e2c2f6c309b139dfc24510eb4cf241704118c26f
Bartu10 <jbg10102002@gmail.com>
2025-01-23 23:19:29 +0100

    users 1.0

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  standalone: true,
+  imports: [HttpClientModule, CommonModule, RouterModule],
+  providers: [ApiService],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  styleUrls: ['./users.component.scss'],
+
 })
 export class UsersComponent {
+
+  constructor(private api: ApiService, private router: Router) { }
+
+  users: any = [];
+
+  ngOnInit(): void {
+    this.api.getUsers().subscribe((data: any) => {
+      this.users = data;
+    });
+  }
+
+  goToUserTasks(userId: number){
+    this.router.navigate(['/tasks', userId]);
+
+  }
+
+
+  openLocation(lat: string, lng: string): void {
+    const url = `https://www.google.com/maps?q=${lat},${lng}`;
+    window.open(url, '_blank'); // Abrir en una nueva pestaña
+  }
+
 
 }
