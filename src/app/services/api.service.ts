939765d6bd3638a19dd18d504709e7d29d412d16
Bartu10 <jbg10102002@gmail.com>
2025-01-23 22:32:45 +0100

    api-calls

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../enviroments/enviroment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+
+    getUsers(){
+      return this.http.get(`${environment.apiUrl}/users`);
+    }
+
+    getTasks(){
+      return this.http.get(`${environment.apiUrl}/todos`);
+    }
+
+    getUserTasks(userId: number){
+      return this.http.get(`${environment.apiUrl}/todos?userId=${userId}`);
+    }
+
+
+
+
+}
