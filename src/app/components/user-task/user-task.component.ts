846ef1a169234495026f86f616058766cc8ad949
Bartu10 <jbg10102002@gmail.com>
2025-01-25 01:32:55 +0100

    userTasks final version

--- src/app/components/user-task/user-task.component.ts
+++ src/app/components/user-task/user-task.component.ts
@@ -14,34 +14,71 @@ import { CommonModule } from '@angular/common';
 export class UserTaskComponent {
 
 
-    constructor(private api: ApiService, private router: Router) { }
+    constructor(public api: ApiService, private router: Router) { }
 
     users: any = [];
-    tasks: any = []
+    tasks: any = [];
+    filteredTasks: any = [];
+    userComplete: any
+    completedTasks: number = 0;
+    pendingTasks: number = 0;
+    filterSelected: string = 'all';
 
 
     ngOnInit(): void {
 
-
-
-
+      if (typeof window !== 'undefined' && localStorage) {
+        const user = localStorage.getItem('user');
+        this.userComplete = user ? JSON.parse(user) : null;
+      } else {
+        console.warn('localStorage no está disponible en este entorno.');
+        this.userComplete = null;
+      }
       const userId = this.router.url.split('/').pop();
       if (userId) {
         this.api.getUserTasks(userId).subscribe((tasks: any) => {
           this.tasks = tasks;
+          this.updateTaskCounts();
+          this.filterTasks('all');
         });
       }
 
-      this.api.getUsers().subscribe((data: any) => {
-        this.users = data;
-      });
     }
+
+
+    updateTaskCounts(): void {
+      this.completedTasks = this.tasks.filter((task: any) => task.completed).length;
+      this.pendingTasks = this.tasks.filter((task: any) => !task.completed).length;
+    }
+
 
     toggleTaskStatus(task: any): void {
       task.completed = !task.completed;
+      this.updateTaskCounts();
+      this.filterTasks(this.filterSelected);
+
+    }
+    filterTasks(filter: string): void {
+      this.filterSelected = filter;
+      if (filter === 'all') {
+        this.filteredTasks = [...this.tasks];
+      } else if (filter === 'completed') {
+        this.filteredTasks = this.tasks.filter((task: any) => task.completed);
+      } else if (filter === 'pending') {
+        this.filteredTasks = this.tasks.filter((task: any) => !task.completed);
+      }
     }
 
-
+    sortTasks(event: Event): void {
+      const criteria = (event.target as HTMLSelectElement).value;
+      if (criteria === 'alphabetical') {
+        this.filteredTasks.sort((a: any, b: any) => a.title.localeCompare(b.title));
+      } else if (criteria === 'completed') {
+        this.filteredTasks.sort((a: any, b: any) => (b.completed ? 1 : -1) - (a.completed ? 1 : -1));
+      } else if (criteria === 'pending') {
+        this.filteredTasks.sort((a: any, b: any) => (a.completed ? 1 : -1) - (b.completed ? 1 : -1));
+      }
+    }
 
 
 }
